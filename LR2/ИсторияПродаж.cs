2f9086109448a01a442313385df33464fadd51c6
Катя <20katerina0060@gmail.com>
2024-10-24 09:36:46 +0300

    Создали класс для описания товаров, перечисление спортивных товаров, описание истории продаж, описание записи продаж

---
+++ LR2/ИсторияПродаж.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    internal class ИсторияПродаж
+    {
+        Dictionary<СпортТовары, List<ЗаписьПродаж>> СловарьПродаж = new Dictionary<СпортТовары, List<ЗаписьПродаж>>();
+        
+    }
+}
---
+++ LR2/ОписаниеТоваров.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    internal class ОписаниеТоваров
+    {
+        private string Название;
+        private double Цена;
+        public ОписаниеТоваров(string Название_, double Цена_) 
+        {
+            Название = Название_;
+            Цена = Цена_;
+        }
+        public string СкажиНазвание() 
+        {
+            return Название;
+            
+        }
+         
+        
+         
+
+        
+     
+        
+        
+        
+        
+
+
+    }
+}
