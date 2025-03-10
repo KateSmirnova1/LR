43e73fdb199ee8492a2b0828da724d00b17e033d
Катя <20katerina0060@gmail.com>
2025-03-10 21:35:30 +0300

    Лр3

---
+++ LR3/лр3/лр3/MainForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace лр3
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(52, 44);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Заказать";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(286, 44);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(251, 144);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(333, 225);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ LR3/лр3/лр3/MainForm.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace лр3
+{
+    public partial class MainForm : Form
+    {
+        // Словарь для хранения групп блюд и списка блюд в каждой группе
+        private Dictionary<string, List<string>> menuGroups;
+
+        // Словарь для хранения выбранных блюд и их количества
+        private Dictionary<string, Dictionary<string, int>> selectedDishes;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // Инициализация данных
+            InitializeMenu();
+            InitializeForm();
+        }
+        private void InitializeMenu()
+        {
+            menuGroups = new Dictionary<string, List<string>>
+            {
+                { "Завтраки", new List<string> { "Омлет", "Блинчики", "Каша" } },
+                { "Обеды", new List<string> { "Суп", "Плов", "Салат" } },
+                { "Ужины", new List<string> { "Стейк", "Рыба", "Паста" } }
+            };
+
+            // Инициализация словаря для хранения выбранных блюд
+            selectedDishes = new Dictionary<string, Dictionary<string, int>>();
+            foreach (var group in menuGroups.Keys)
+            {
+                selectedDishes[group] = new Dictionary<string, int>();
+            }
+        }
+
+        private void InitializeForm()
+        {
+            // Заполнение ComboBox группами блюд
+            foreach (var group in menuGroups.Keys)
+            {
+                comboBox1.Items.Add(group);
+            }
+
+            // Установка обработчика события выбора группы
+            comboBox1.SelectedIndexChanged += ComboBoxMenuGroups_SelectedIndexChanged;
+
+            // Установка обработчика события нажатия на кнопку "Заказать"
+            button1.Click += Button1_Click;
+        }
+
+        private void ComboBoxMenuGroups_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Очистка предыдущих элементов
+            flowLayoutPanel1.Controls.Clear();
+
+            // Получение выбранной группы
+            string selectedGroup = comboBox1.SelectedItem.ToString();
+
+            // Добавление CheckBox и NumericUpDown для каждого блюда в выбранной группе
+            foreach (var dish in menuGroups[selectedGroup])
+            {
+                CheckBox checkBox = new CheckBox { Text = dish };
+                NumericUpDown numericUpDown = new NumericUpDown { Minimum = 0, Maximum = 10, Width = 50 };
+
+                // Установка значения NumericUpDown, если блюдо уже было выбрано ранее,то извлекается кол-во и отмечается галочкой 
+                if (selectedDishes[selectedGroup].ContainsKey(dish))
+                {
+                    numericUpDown.Value = selectedDishes[selectedGroup][dish];
+                    checkBox.Checked = true;
+                }
+
+                // Обработчик изменения состояния CheckBox
+                checkBox.CheckedChanged += (s, ev) =>
+                {
+                    if (checkBox.Checked)
+                    {
+                        selectedDishes[selectedGroup][dish] = (int)numericUpDown.Value;
+                    }
+                    else
+                    {
+                        selectedDishes[selectedGroup].Remove(dish);
+                    }
+                };
+
+                // Обработчик изменения значения NumericUpDown
+                numericUpDown.ValueChanged += (s, ev) =>
+                {
+                    if (checkBox.Checked)
+                    {
+                        selectedDishes[selectedGroup][dish] = (int)numericUpDown.Value;
+                    }
+                };
+
+                flowLayoutPanel1.Controls.Add(checkBox);
+                flowLayoutPanel1.Controls.Add(numericUpDown);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // Формирование заказа
+            string order= "Ваш заказ:\n";
+
+            foreach (var group in menuGroups.Keys)
+            {
+                order += $"{group}:\n";
+
+                // Если в группе есть выбранные блюда
+                if (selectedDishes[group].Count > 0)
+                {
+                    foreach (var dish in selectedDishes[group])
+                    {
+                        order += $"- {dish.Key} x {dish.Value}\n";
+                    }
+                }
+                else
+                {
+                    // Если блюда не выбраны, добавляем прочерк
+                    order += "- \n";
+                }
+            }
+
+            // Вывод заказа на форму
+            label1.Text = order;
+        }
+    }
+}
