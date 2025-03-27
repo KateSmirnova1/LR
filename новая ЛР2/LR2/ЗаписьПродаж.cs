efad207425b282513a938cdffaf3cca6f640d99b
Катя <20katerina0060@gmail.com>
2025-03-27 12:32:03 +0300

    Сделала ЛР2

---
+++ новая ЛР2/LR2/ЗаписьПродаж.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    public struct ЗаписьПродаж
+    {
+        public int День; 
+        public int Количество { get; set; }
+        public ОписаниеТоваров Товар { get; set; }
+
+        // Конструктор
+        public ЗаписьПродаж(ОписаниеТоваров товар, int день, int количество)
+        {
+            Товар = товар;
+            День = день;
+            Количество = количество;
+        } 
+        
+        public int GetDay ()
+        {
+            return День;
+        }
+
+        public int GetKol()
+        {
+            return Количество ;
+        }
+        public ОписаниеТоваров GetGoods()
+        {
+            return Товар;
+        }
+
+
+        
+
+    }
+}
---
+++ новая ЛР2/LR2/ОписаниеТоваров.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR2
+{
+    public class ОписаниеТоваров
+    {
+        private string Название { get; set; }
+        private double Цена { get;  set; }
+        private СпортТовары Группа { get;  set; }
+
+        // Конструктор
+        public ОписаниеТоваров(string название, double цена, СпортТовары группа)
+        {
+            Название = название;
+            Цена = цена;
+            Группа = группа;
+        }
+        public double GetPrice()
+        {
+            return Цена;
+        }
+        public string GetTitle()
+        {
+            return Название;
+        }
+        public СпортТовары GetGroup()
+        {
+            return Группа;
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
